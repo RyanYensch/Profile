38b3668835fb9e323ea359913a61ccc1e97dca95
RyanYensch <ry2005@outlook.com>
2025-05-25 07:08:29 +1000

    magnet updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".magnet").forEach(mag => {
-        mag.addEventListener("click", () => {
-            const panel = document.getElementById(mag.dataset.target);
-            mag.classList.toggle("open");
-            panel.classList.toggle("open");
-        })
-    })
+document.querySelectorAll(".magnet-icon").forEach(mag => {
+    mag.addEventListener("click", () => {
+        const target = mag.dataset.target;
+        const dir = mag.dataset.dir;
+        const panel = document.getElementById(target);
+        const label = document.querySelector(`.label[data-target="${target}"]`);
+        const panelHeight = panel.getBoundingClientRect().panelHeight;
+
+        if (!panel.classList.contains("open")) {
+            panel.classList.add("open");
+            const shift = (dir == "up") ? -panelHeight : panelHeight;
+
+            mag.style.transform = `translateY(${shift}px) ${(dir == "down") ? "rotate(180deg)" : ""}`;
+            label.style.transform = `translateY(${shift}px)`;
+        } else {
+            panel.classList.remove("open");
+            mag.style.transform ="";
+            label.style.transform = "";
+        }
+        
+    });
 });
