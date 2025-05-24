a450bfae8c4413d2fb341becf270bc80cb1cdc6f
RyanYensch <ry2005@outlook.com>
2025-05-25 07:55:48 +1000

    added content loader

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-document.querySelectorAll(".toggle-group").forEach(group => {
-    group.addEventListener("click", () => {
-        const target = group.dataset.target;
-        const dir = group.dataset.dir;
-        const panel = document.getElementById(target);
-        const panelHeight = panel.getBoundingClientRect().height;
-        const offset = (dir == "up") ? -panelHeight : panelHeight;
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll(".toggle-group").forEach(group => {
+        group.addEventListener("click", () => {
+            const target = group.dataset.target;
+            const dir = group.dataset.dir;
+            const panel = document.getElementById(target);
+            const panelHeight = panel.getBoundingClientRect().height;
+            const offset = (dir == "up") ? -panelHeight : panelHeight;
 
-        panel.classList.toggle("open")
-        if (panel.classList.contains("open")) {
-            group.style.setProperty("--offset", `${offset}px`);
-        } else {
-            group.style.setProperty("--offset", "0px");
-        }
-        
+            panel.classList.toggle("open")
+            if (panel.classList.contains("open")) {
+                group.style.setProperty("--offset", `${offset}px`);
+            } else {
+                group.style.setProperty("--offset", "0px");
+            }
+            
+        });
     });
 });
